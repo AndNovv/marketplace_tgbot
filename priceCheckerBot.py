750a5c85cbf74d5fbb1a129d52daa43a1c0ae096
AndNovv <andrew12011976@mail.ru>
2024-09-19 13:11:10 +0300

    Feature: Tracking products with certain size

--- priceCheckerBot.py
+++ priceCheckerBot.py
@@ -1,6 +1,8 @@
 import requests
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import (
+    Application, CommandHandler, ContextTypes, MessageHandler, filters
+)
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from apscheduler.triggers.cron import CronTrigger
@@ -13,8 +15,8 @@ from dotenv import load_dotenv
 # Загружаем переменные окружения из файла .env
 load_dotenv()
 
-# Вставьте свой токен сюда
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+PRODUCTION = os.getenv('PRODUCTION')
 
 mongodb_uri = os.getenv('MONGODB_URI')
 
@@ -38,19 +40,21 @@ def add_user_if_not_exists(chat_id):
     if not existing_user:
         users_collection.insert_one({'chat_id': chat_id, 'followed_products': []})
 
-# Функция для добавления товара в массив followed_products пользователя
-def follow_product(chat_id, product_id, name, price):
+def follow_product(chat_id, product_id, name, price, size=None):
     add_user_if_not_exists(chat_id)
     users_collection.update_one(
         {'chat_id': chat_id},
         {'$addToSet': {'followed_products': {
-            'product_id': product_id, 
-            'name': name, 
-            'lastprice': price, 
-            'has_changed': False,  # Инициализируем флаг has_changed как false
-            'last_updated': datetime.now() 
+            'product_id': product_id,
+            'name': name,
+            'lastprice': price,
+            'size': size,
+            'previous_price': price,
+            'has_changed': False,
+            'last_updated': datetime.now()
         }}}
     )
+
 # Функция для удаления товара из массива followed_products
 def unfollow_product(chat_id, product_id):
     users_collection.update_one(
@@ -69,7 +73,7 @@ def get_user_products(chat_id):
         return user['followed_products']
     return []
 
-# Функция для обновления данных о товарах
+
 def update_product_data():
     print('Обновление данных о продуктах')
     all_users = get_all_users()
@@ -83,40 +87,62 @@ def update_product_data():
     if not unique_product_ids:
         return
 
+    # Формируем запрос с артикулами
     article_list = ';'.join(map(str, unique_product_ids))
     url = f'https://card.wb.ru/cards/v2/detail?appType=1&curr=rub&dest=-1257786&locale=ru&spp=30&lang=ru&ab_testing=false&nm={article_list}'
-
+    
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
         products_data = data['data']['products']
 
+        # Обновляем данные о товарах для всех пользователей
         for product_data in products_data:
             product_id = product_data['id']
             name = product_data['name']
-            new_price = product_data['sizes'][0]['price']['total'] / 100
 
+            # Итерируем по пользователям, которые отслеживают данный товар
             users = users_collection.find({'followed_products.product_id': product_id})
             for user in users:
                 for followed_product in user['followed_products']:
                     if followed_product['product_id'] == product_id:
-                        old_price = followed_product.get('lastprice')
-                        previous_price = followed_product.get('previous_price', old_price)
+                        size_to_track = followed_product.get('size')
+                        old_price = followed_product['lastprice']
                         has_changed = followed_product.get('has_changed', False)
 
+                        # Если размер указан, ищем цену конкретного размера
+                        if size_to_track:
+                            # Ищем цену для выбранного размера
+                            new_price = None
+                            for size in product_data['sizes']:
+                                if size['origName'] == size_to_track:
+                                    new_price = size['price']['total'] / 100
+                                    break
+
+                            if new_price is None:
+                                print(f"Размер {size_to_track} не найден для товара {name}. Пропускаем.")
+                                continue
+                        else:
+                            # Если размер не указан, берем цену первого размера
+                            new_price = product_data['sizes'][0]['price']['total'] / 100
+
+                        # Проверяем изменение цены
                         if new_price != old_price:
                             has_changed = True
+                            price_difference = new_price - old_price
+                            percentage_change = (price_difference / old_price) * 100
                         else:
                             has_changed = False
 
+                        # Обновляем товар для пользователя
                         users_collection.update_one(
                             {'chat_id': user['chat_id'], 'followed_products.product_id': product_id},
                             {'$set': {
                                 'followed_products.$.name': name,
                                 'followed_products.$.lastprice': new_price,
+                                'followed_products.$.previous_price': old_price,  # Сохраняем старую цену
                                 'followed_products.$.has_changed': has_changed,
-                                'followed_products.$.previous_price': previous_price,
                                 'followed_products.$.last_updated': datetime.now()
                             }}
                         )
@@ -228,25 +254,73 @@ async def follow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         try:
             article_number = int(context.args[0])
 
+            # Формируем запрос на получение информации о товаре
             url = f'https://card.wb.ru/cards/v2/detail?appType=1&curr=rub&dest=-1257786&locale=ru&spp=30&lang=ru&ab_testing=false&nm={article_number}'
             response = requests.get(url)
             data = response.json()
-            
+
             try:
                 product = data['data']['products'][0]
                 name = product['name']
-                price = product['sizes'][0]['price']['total'] / 100
-                follow_product(update.message.chat_id, article_number, name, price)
-                await update.message.reply_text(f'Артикул {article_number} добавлен в список.')
+                sizes = product.get('sizes', [])
+
+                # Проверяем, есть ли у товара несколько размеров
+                if len(sizes) > 1:
+                    # Отправляем пользователю выбор размеров
+                    size_options = [f"{size['origName']}" for size in sizes]
+                    size_keyboard = [[option] for option in size_options]
+                    reply_markup = ReplyKeyboardMarkup(size_keyboard, one_time_keyboard=True)
+                    
+                    # Сохраняем артикул и размеры в контексте для дальнейшей обработки
+                    context.user_data['article_number'] = article_number
+                    context.user_data['name'] = name
+                    context.user_data['sizes'] = sizes
+                    context.user_data['awaiting_size_selection'] = True  # Устанавливаем флаг для ожидания выбора размера
+                    
+                    await update.message.reply_text(
+                        'Этот товар доступен в нескольких размерах. Пожалуйста, выберите размер для отслеживания:',
+                        reply_markup=reply_markup
+                    )
+                else:
+                    # Если размер один, сразу добавляем товар
+                    price = sizes[0]['price']['total'] / 100
+                    follow_product(update.message.chat_id, article_number, name, price)
+                    await update.message.reply_text(f'Артикул {article_number} добавлен в список.')
+
             except (KeyError, IndexError) as e:
                 print(f"Ошибка при извлечении данных о товаре: {e}")
-
-            # Добавляем товар и пользователя в базу данных
+                await update.message.reply_text('Ошибка при получении данных о товаре.')
+                
         except ValueError:
             await update.message.reply_text('Пожалуйста, введите действительный артикул товара.')
     else:
         await update.message.reply_text('Пожалуйста, укажите артикул товара.')
 
+# Обработчик для выбора размера
+async def handle_size_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if context.user_data.get('awaiting_size_selection'):
+        size_selected = update.message.text
+        sizes = context.user_data.get('sizes', [])
+        article_number = context.user_data.get('article_number')
+        name = context.user_data.get('name')
+
+        # Ищем выбранный размер среди доступных
+        selected_size = next((size for size in sizes if size_selected.startswith(size['origName'])), None)
+        print(selected_size)
+
+        if selected_size:
+            sizeName = f"{selected_size['origName']}"
+            try:
+                price = selected_size['price']['total'] / 100
+                follow_product(update.message.chat_id, article_number, name, price, size=sizeName)
+                await update.message.reply_text(f'{name} с размером {sizeName} добавлен в список отслеживаемых.')
+            except:
+                await update.message.reply_text(f'{name} с размером {sizeName} нет в наличии.')
+        else:
+            await update.message.reply_text('Выбранный размер не найден.')
+        
+        # Сбрасываем флаг ожидания выбора размера
+        context.user_data['awaiting_size_selection'] = False
 
 async def unfollow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if context.args:
@@ -288,10 +362,15 @@ async def check_followed_products(update: Update, context: ContextTypes.DEFAULT_
     for product in followed_products:
         product_id = product['product_id']
         name = product['name']
+        size = product.get('size', None)
         price = product['lastprice']
     
-        messages.append(f'Артикул: {product_id}.\nТовар: {name}\nЦена: {price} руб.\nСсылка: https://www.wildberries.ru/catalog/{product_id}/detail.aspx')
-    
+        messages.append(f'Артикул: {product_id}.\n'
+                        f'Товар: {name}\n'
+                        f'Размер: {size}\n'
+                        f'Цена: {price} руб.\n'
+                        f'Ссылка: https://www.wildberries.ru/catalog/{product_id}/detail.aspx')
+        
     message = '\n\n'.join(messages)
     await update.message.reply_text(message, disable_web_page_preview=True)
 
@@ -303,13 +382,15 @@ def main() -> None:
     # Настраиваем планировщик
     scheduler = AsyncIOScheduler()
 
-    # Каждые 24 часа начиная с запуска бота
-    scheduler.add_job(send_update_to_users, trigger=IntervalTrigger(minutes=1), args=[application])
-
-    # Каждый день в 11 утра
-    # scheduler.add_job(send_update_to_users, trigger=CronTrigger(hour=11, minute=0), args=[application])
+    if PRODUCTION == True:
+        # Каждый день в 11 утра
+        scheduler.add_job(send_update_to_users, trigger=CronTrigger(hour=11, minute=0), args=[application])
+    else:
+        # Каждую минуту начиная с запуска бота
+        scheduler.add_job(send_update_to_users, trigger=IntervalTrigger(minutes=1), args=[application])
+    
     scheduler.start()
-
+    
     # Регистрируем обработчики команд
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
@@ -318,6 +399,9 @@ def main() -> None:
     application.add_handler(CommandHandler("unfollow", unfollow))
     application.add_handler(CommandHandler("clear", clear))
     application.add_handler(CommandHandler("check", check_followed_products))
+
+    # Обработчик для текстовых сообщений (для обработки выбора размера)
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_size_selection))
 
     # Запускаем бота
     application.run_polling()
